6e226a2fa71ece72d479ad6561b82072836bfb3c
Rob Fisher <rfisher@sevone.com>
2017-05-08 13:48:48 -0400

    Auto-calculate Next Meeting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,30 @@
 function getDate(month) {
-  var result = month.endOf('month');
-  while (result.day() !== 1) {
-    result.subtract(1, 'day');
-  }
+  // Get the first Thursday of the month.
+  var result = month.startOf('month').day('Thursday');
+  // Add a week to get the second Thursday of the month.
+  result.add(1, 'week');
+  // Add 18.5 hours to set the date
+  result.add(18, 'hours');
 
   return result;
 }
 
 function nextMeeting() {
-  /*var result = getDate(moment());
-  var dur = result.diff(moment);
+  // Get the next meeting date
+  var meeting = getDate(moment());
 
-  if (dur < 0) {
-    result = getDate(moment().add(1, 'month'));
+  // If meeting has passed this month then look for next month's meeting
+  if(meeting.isBefore(moment())) {
+    var nextMonth = moment().add(1, 'month');
+    meeting = getDate(nextMonth);
   }
 
-  var meeting = result.subtract(5.5, 'hours');
-  meeting.add(1, 'second');*/
-
-  var meeting = moment('2017-05-11 18:00:00');
-
+  // Show the pretty date in the text
   $('.next').text(meeting.format('dddd, MMMM Do YYYY, h:mm:ss a'));
 
   $('.countdown').downCount({
-    date: meeting.add(4, 'hours').format(),
-    offset: 0
+    date: meeting,
+    offset: -4
   });
 }
 
